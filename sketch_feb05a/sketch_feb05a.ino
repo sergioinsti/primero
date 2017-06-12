7eff2d87336a37d1c333d8dad843b8b7422d2904
sergioinsti <sergio10500@yahoo.es>
2017-06-12 19:40:46 +0200

    Add files via upload

---
+++ sketch_feb05a/sketch_feb05a.ino
@@ -0,0 +1,13 @@
+int ledPin = 13;     // LED que se encuentra en el pin 13
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(ledPin, OUTPUT);   // Establece 'pin' como salida 
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+   digitalWrite(ledPin, HIGH);  // Activa 'pin'
+   delay(1000);     // Pausa un segundo 
+   digitalWrite(ledPin, LOW);  // Desactiva 'pin'
+   delay(1000);
+}
